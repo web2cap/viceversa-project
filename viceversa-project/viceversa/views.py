695d188d50bf8e43629c4adc4c3fed96d654c45f
web2cap <web2cap@gmail.com>
2021-06-13 22:52:59 +0300

    reverse text

--- viceversa-project/viceversa/views.py
+++ viceversa-project/viceversa/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def reverse(request):
+    reversed = request.POST['usertext'][::-1]
+    data = {'reversed' : reversed}
+    return render(request,'reverse.html',data)
